40da75ff848440bf1535a603e53a3800fd96c8b5
nin0dev <personal@nin0dev.com>
2024-05-11 03:19:40 -0400

    Added lanyard presence

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-shouldLog = false;
+let shouldLog = false;
 function log(content) {
     if (shouldLog) console.log(content);
 }
@@ -20,6 +20,31 @@ function onUpdate(data) {
             pfp.style.borderColor = "var(--offline-color)";
             break;
     }
+    // set presence
+    log(data["activities"])
+    let listening = false
+    let content = ""
+    data["activities"].forEach(presence => {
+        if(presence["application_id"] == "463151177836658699" && presence["assets"]["small_text"] != "Paused") { // premid
+            listening = true
+            artist = presence["state"].substring(0, presence["state"].indexOf(" -"))
+            if (artist == "") {
+                artist = presence["state"]
+            }
+            content = `Listening to ${presence["details"]} - ${artist}`
+        }
+        if(presence["application_id"] == "1108588077900898414") { // vencord lastfm
+            listening = true
+            content = `Listening to ${presence["details"]} - ${presence["state"]}`
+        }
+        if(presence["type"] == 0 && presence["application_id"] != "463151177836658699" && presence["application_id"] != "1108588077900898414") { // generic playing status that isn't vencord lastfm or premid
+            listening = false
+            content = `Playing ${presence["name"]}`
+        }
+    });
+    document.getElementById("presence").style.display = content == "" ? "none": "flex"
+    document.getElementById("presence-content").innerText = content
+    document.getElementById("presence-icon").src = listening ? "music.png" : "game.png"
 }
 LanyardWrapper.connectWebSocket("886685857560539176", onUpdate)
 .catch(err => {

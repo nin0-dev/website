f05c6af3ba9fd862f1550499a970976409ab0d54
nin0dev <personal@nin0dev.com>
2024-05-12 10:41:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function onUpdate(data) {
         }
         if(presence["application_id"] == "1054951789318909972") { // vendetta lastfm
             listening = true
-            content = `Listening to ${presence["details"]} - ${presence["state"].replace("by ", "")}`
+            content = `Listening to ${presence["details"]} - ${presence["state"]}`
         }
         if(presence["type"] == 0 && presence["application_id"] != "463151177836658699" && presence["application_id"] != "1108588077900898414") { // generic playing status that isn't vencord lastfm or premid
             listening = false
